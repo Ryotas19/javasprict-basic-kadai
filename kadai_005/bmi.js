3625aa261eb0829bc3d3d70d19efe5f07c1706ef
Ryotas19 <ryotassoccer19@gmail.com>
2025-04-25 14:05:45 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重と身長の変数を定義
+let weight = 68;        
+let height = 1.7;       
+
+// BMIの計算
+let bmi = weight / (height * height);
+
+// コンソールに出力
+console.log("体重：" + weight + "kg");
+console.log("身長：" + height + "m");
+console.log("BMI：" + bmi);
